39f2592f00b4b3d43d03fe02c35792ed291d0bd1
Atle Olson <atle95@gmail.com>
2016-06-08 12:09:44 -0600

    map update

--- HappyGame/src/procedrualmap/MapTile.java
+++ HappyGame/src/procedrualmap/MapTile.java
@@ -9,28 +9,41 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class mapTile extends Application
+public class MapTile extends Application
 {
 	int xindex;
 	int yindex;
-	public int width = 800;
-	public int height = 800;
+	public int width = 256;
+	public int height = 256;
 	Image tileImage;
 	Canvas canvas;
+	
 
-	public mapTile()
+	public MapTile()
 	{
 //		tileImage = new Image("", width, height, false, false);
-		canvas = new Canvas(width, height);
-		GraphicsContext gfx = canvas.getGraphicsContext2D();
-		gfx.setFill(Color.GREEN);
-		gfx.fillRect(10, -10, 400, 200);
+		
+		
+//		gfx.setFill(Color.GREEN);
+//		gfx.fillRect(10, -10, 400, 200);
+		
 		
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception 
 	{
+//		GraphicsContext gfx;
+//		canvas = new Canvas(width, height);
+//		gfx = canvas.getGraphicsContext2D();
+//		for(int i = 0; i< width;i++)
+//		{
+//			for(int j = 0; j< height;j++)
+//			{
+//				gfx.setFill(Color.rgb(i, j, 100));
+//				gfx.fillRect(i,j,2,2);
+//			}			
+//		}
 		StackPane pane = new StackPane(canvas);
 		Scene scene = new Scene(pane, width, height);
 		primaryStage.setScene(scene);
